94ea9c62d7206ac69bfb05715997926638034347
Andre Herman <amelco.herman@gmail.com>
2025-10-14 17:11:13 -0300

    mend

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ HEADERS="-I. -I./headers -I./src"
 CFG="-fPIC -shared"
 LIBS="-L. -L./bin -L./libraries -l:libraylib.so -lm"
 
+#gcc $CFLAGS $HEADERS -o ./bin/mg_test ./examples/mg_test.c $LIBS -l:libgrid.so -Wl,-rpath=./libraries -Wl,-rpath=./bin
+
 gcc $CFLAGS $HEADERS $CFG -o ./bin/lib$1.so ./examples/$1_plug.c $LIBS \
     -march=native -ffast-math -flto=auto -lpthread
 gcc $CFLAGS $HEADERS -o ./bin/$1 ./examples/$1.c $LIBS -l:libgrid.so -Wl,-rpath=./libraries -Wl,-rpath=./bin \
